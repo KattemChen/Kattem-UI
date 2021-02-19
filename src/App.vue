9b4e2e6c3b0b775693909b63439cbe1d24903175
KattemChen <kattem.chen@chiefclouds.com>
2021-02-19 14:56:02 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Kettem></Kettem>
+</template>
+
+<script>
+import Kettem from './components/Kettem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Kettem
+  }
+}
+</script>
---
+++ src/components/Kettem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>hi~</div>
+</template>
+
+<script>
+export default {
+  name: 'Kettem',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
