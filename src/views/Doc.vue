c1b89c7edde36d4901c32200da96f5887a90b09c
KattemChen <kattem.chen@chiefclouds.com>
2021-02-20 11:28:48 +0800

    Topnav抽离

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,10 +1,20 @@
 <template>
+  <Topnav></Topnav>
   <div class="topNav">
-      <div class="logo"></div>
-      <div class="menu"></div>
-    </div>
-    <div class="content">
-      <aside>侧边栏</aside>
-      <main>内容</main>
-    </div>
+    <div class="logo"></div>
+    <div class="menu"></div>
+  </div>
+  <div class="content">
+    <aside>侧边栏</aside>
+    <main>内容</main>
+  </div>
 </template>
+<script>
+import Topnav from '../components/Topnav.vue'
+
+export default {
+  components: {
+    Topnav
+  }
+}
+</script>
